dbc20e174be3c3ac54637efa9616b7f91980b9a9
zTsugumi <nguyenngochai.tin47@gmail.com>
2023-12-05 18:38:01 +0700

    Fix random_search - log to file

--- nas/search_algo/random_search.py
+++ nas/search_algo/random_search.py
@@ -25,45 +25,54 @@ class RandomSearch:
             if efficiency <= self.efficiency_constraint:
                 return spec, efficiency
 
-    def run_compare(self):
+    def run_compare(self, plot=False):
         efficiency = {
             'Est': [],
-            'Truth': []
+            'Truth': [],
         }
         x = []
+        diff = []
 
-        for i in range(10):
-            x.append(i)
-            spec = self.arch_manager.random_spec()
-            net = MobileNetV3.build_from_spec(spec, 1000)
+        with open('data/comparison.txt', 'w') as f:
+            for i in range(1000):
+                x.append(i)
+                spec = self.arch_manager.random_spec()
+                net = MobileNetV3.build_from_spec(spec, 1000)
 
-            efficiency['Est'].append(
-                round(self.efficiency_predictor.predict_efficiency_from_spec(spec), 2) / 100)
-            efficiency['Truth'].append(
-                round(self.efficiency_predictor.predict_efficiency_from_net(
-                    net, spec.get('r', [224])[0]), 2))
+                efficiency['Est'].append(
+                    round(self.efficiency_predictor.predict_efficiency_from_spec(spec), 2) / 100)
+                efficiency['Truth'].append(
+                    round(self.efficiency_predictor.predict_efficiency_from_net(
+                        net, spec.get('r', [224])[0]), 2))
+                diff.append(
+                    abs(efficiency['Est'][-1] - efficiency['Truth'][-1]))
 
-            print(
-                f'Est: {efficiency["Est"][-1]:.2f}\tTruth: {efficiency["Truth"][-1]:.2f}')
+                f.write(
+                    f'Try {i} - Est: {efficiency["Est"][-1]:.2f}\tTruth: {efficiency["Truth"][-1]:.2f}\n')
 
-        import matplotlib.pyplot as plt
-        import numpy as np
+            import numpy as np
+            mae = np.mean(diff)
+            f.write(f'MAE = {mae}')
 
-        x = np.arange(len(x))  # the label locations
-        width = 0.25  # the width of the bars
-        multiplier = 0
+        if plot:
+            import matplotlib.pyplot as plt
+            import numpy as np
 
-        fig, ax = plt.subplots(layout='constrained')
-        for attribute, measurement in efficiency.items():
-            offset = width * multiplier
-            rects = ax.bar(x + offset, measurement, width, label=attribute)
-            ax.bar_label(rects, padding=3)
-            multiplier += 1
+            x = np.arange(len(x))  # the label locations
+            width = 0.25  # the width of the bars
+            multiplier = 0
 
-        ax.set_ylabel('Latency (ms)')
-        ax.set_title('Latency Comparison')
-        ax.set_xticks(x + width, x)
-        ax.legend(loc='upper left', ncols=3)
+            fig, ax = plt.subplots(layout='constrained')
+            for attribute, measurement in efficiency.items():
+                offset = width * multiplier
+                rects = ax.bar(x + offset, measurement, width, label=attribute)
+                ax.bar_label(rects, padding=3)
+                multiplier += 1
 
-        plt.show()
-        fig.savefig('data/comparison.png')
+            ax.set_ylabel('Latency (ms)')
+            ax.set_title('Latency Comparison')
+            ax.set_xticks(x + width, x)
+            ax.legend(loc='upper left', ncols=3)
+
+            plt.show()
+            fig.savefig('data/comparison.png')
